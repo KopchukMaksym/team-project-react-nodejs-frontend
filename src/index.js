d0a91fce012e95085e9210c631a46df179721fde
Maks <104761722+KopchukMaksym@users.noreply.github.com>
2023-03-09 08:06:33 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 
 import { persistedStore, store } from './store';
 import { ModalProvider } from './components/ModalRework';
@@ -17,11 +17,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistedStore}>
-        <BrowserRouter>
+        <HashRouter>
           <ModalProvider>
             <App />
           </ModalProvider>
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
